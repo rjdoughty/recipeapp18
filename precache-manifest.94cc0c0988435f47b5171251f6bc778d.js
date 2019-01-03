31069b5670be6bce3c589703e24398fb0dd06275
DoughBoy <41695186+rjdoughty@users.noreply.github.com>
2019-01-02 19:07:19 -0500

    Updates

---
+++ precache-manifest.94cc0c0988435f47b5171251f6bc778d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55127d5b997531d0cc6d",
+    "url": "/recipeapp18/static/js/runtime~main.55127d5b.js"
+  },
+  {
+    "revision": "6267be21a15b0002aa18",
+    "url": "/recipeapp18/static/js/main.6267be21.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipeapp18/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "6267be21a15b0002aa18",
+    "url": "/recipeapp18/static/css/main.e1d20411.chunk.css"
+  },
+  {
+    "revision": "f7037172297a973ee82d9fbcff496825",
+    "url": "/recipeapp18/index.html"
+  }
+];
